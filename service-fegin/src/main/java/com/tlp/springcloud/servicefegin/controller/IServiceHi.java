dcb8fe3668aa8e0db68e1321942b18e1a63b0dd0
tianlingpeng <1194833510@qq.com>
2019-09-28 12:13:17 +0800

    修改依赖

--- service-fegin/src/main/java/com/tlp/springcloud/servicefegin/controller/IServiceHi.java
+++ service-fegin/src/main/java/com/tlp/springcloud/servicefegin/controller/IServiceHi.java
@@ -1,6 +1,7 @@
 package com.tlp.springcloud.servicefegin.controller;
 
-import org.springframework.cloud.netflix.feign.FeignClient;
+
+import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 
 /**
